e14b48b67d0acdfbc36484b34d2b3f78fb23eb36
Jolmos4 <jorgeolmosloya10@hotmail.com>
2023-09-26 12:50:05 -0600

    code 4 ATTEMPT #5
    added str before year

--- code_4.py
+++ code_4.py
@@ -6,21 +6,20 @@ df = pd.read_csv(big_mac_file)
 
 def get_big_mac_price_by_year(year, country_code):
     country_code = country_code.upper()
-    df_by_date = df[df['date'].str.startswith(year) & (df['iso_a3']== country_code)]
+    df_by_date = df[df['date'].str.startswith(str(year)) & (df['iso_a3']== country_code)]
     # new_query = f"(date > '{year}-04-01' & iso_a3 == '{country_code}')"
     mean_dollar_price = round(df_by_date['dollar_price'].mean(), 2)
     return mean_dollar_price
 
 def get_big_mac_price_by_country(country_code):
     country_code = country_code.upper()
-    query = f"(iso_a3 == '{country_code}')"
-    df_by_country = df.query(query)
+    df_by_country = df[df['iso_a3']== country_code]
     return round(df_by_country['dollar_price'].mean(), 2)
 
 def get_the_cheapest_big_mac_price_by_year(year):
     #new_query = f"(date >= '{year}-01-01' & date <= '{year}-12-31')"
     #df_by_year = df.query(new_query)
-    df_by_year = df[df['date'].str.startswith(year)]
+    df_by_year = df[df['date'].str.startswith(str(year))]
     cheapest_price = df_by_year['dollar_price'].min()
     cheapest_row = df_by_year[df_by_year['dollar_price'] == cheapest_price].iloc[0]
     cheapest_country_name = cheapest_row['country_name']
@@ -30,7 +29,7 @@ def get_the_cheapest_big_mac_price_by_year(year):
 def get_the_most_expensive_big_mac_price_by_year(year):
     # new_query = f"(date >= '{year}-01-01' & date <= '{year}-12-31')"
     # df_by_year = df.query(new_query)
-    df_by_year = df[df['date'].str.startswith(year)]
+    df_by_year = df[df['date'].str.startswith(str(year))]
     expensive_price = df_by_year['dollar_price'].max()
     expensive_row = df_by_year[df_by_year['dollar_price'] == expensive_price].iloc[0]
     expensive_country_name = expensive_row['country_name']
@@ -48,7 +47,7 @@ if __name__ == "__main__":
     print(f"Overall Mean Dollar Price for {country_code.upper()}: ${mean_price_country}")
 
     cheapest_result = get_the_cheapest_big_mac_price_by_year(year)
-    print(f"Cheapest Big Mac in {year}: {cheapest_result}")
+    print(f"Cheapest Price in Big Mac in {year}: {cheapest_result}")
 
     expensive_result = get_the_most_expensive_big_mac_price_by_year(year)
-    print(f"Most Expensive Big Mac in {year}: {expensive_result}")
+    print(f"Most Expensive Price in Big Mac in {year}: {expensive_result}")
