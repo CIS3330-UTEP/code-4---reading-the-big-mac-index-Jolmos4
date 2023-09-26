0bd11f77596f7b50b22c492bb18f2fcec09a0a7b
Jolmos4 <jorgeolmosloya10@hotmail.com>
2023-09-26 12:40:11 -0600

    code 4 attempt #4
    added str function

--- code_4.py
+++ code_4.py
@@ -4,15 +4,10 @@ import pandas as pd
 big_mac_file = './big-mac-full-index.csv'
 df = pd.read_csv(big_mac_file)
 
-if 'country_name' in df.columns:
-    df = df.dropna(subset=['country_name'])
-else:
-    print("Warning: 'country_name' column doesn't exist in the DataFrame.")
-
 def get_big_mac_price_by_year(year, country_code):
     country_code = country_code.upper()
-    new_query = f"(date > '{year}-04-01' & iso_a3 == '{country_code}')"
-    df_by_date = df.query(new_query)
+    df_by_date = df[df['date'].str.startswith(year) & (df['iso_a3']== country_code)]
+    # new_query = f"(date > '{year}-04-01' & iso_a3 == '{country_code}')"
     mean_dollar_price = round(df_by_date['dollar_price'].mean(), 2)
     return mean_dollar_price
 
@@ -23,8 +18,9 @@ def get_big_mac_price_by_country(country_code):
     return round(df_by_country['dollar_price'].mean(), 2)
 
 def get_the_cheapest_big_mac_price_by_year(year):
-    new_query = f"(date >= '{year}-01-01' & date <= '{year}-12-31')"
-    df_by_year = df.query(new_query)
+    #new_query = f"(date >= '{year}-01-01' & date <= '{year}-12-31')"
+    #df_by_year = df.query(new_query)
+    df_by_year = df[df['date'].str.startswith(year)]
     cheapest_price = df_by_year['dollar_price'].min()
     cheapest_row = df_by_year[df_by_year['dollar_price'] == cheapest_price].iloc[0]
     cheapest_country_name = cheapest_row['country_name']
@@ -32,8 +28,9 @@ def get_the_cheapest_big_mac_price_by_year(year):
     return f"{cheapest_country_name}({cheapest_country_code}): ${cheapest_price}"
 
 def get_the_most_expensive_big_mac_price_by_year(year):
-    new_query = f"(date >= '{year}-01-01' & date <= '{year}-12-31')"
-    df_by_year = df.query(new_query)
+    # new_query = f"(date >= '{year}-01-01' & date <= '{year}-12-31')"
+    # df_by_year = df.query(new_query)
+    df_by_year = df[df['date'].str.startswith(year)]
     expensive_price = df_by_year['dollar_price'].max()
     expensive_row = df_by_year[df_by_year['dollar_price'] == expensive_price].iloc[0]
     expensive_country_name = expensive_row['country_name']
